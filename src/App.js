c3ec2568fc039497554159aaa4c6fab4883efe91
aselwilliams <aselwilliams22@gmail.com>
2022-05-11 10:39:49 -0400

    markup and styles added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+    <h1>Counter</h1>
+    <span id="counter">0</span>
+    <div className="btn-wrapper">
+        <button className="btn increase">Increase</button>
+        <button className="btn reset">Reset</button>
+        <button className="btn decrease">Decrease</button>
     </div>
+</div>
   );
 }
 
